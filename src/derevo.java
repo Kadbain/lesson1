cfe08ce6a500f048fb0dfad208ccd09cf3515c18
Kadbain <ps1x0209@gmail.com>
2016-10-11 16:46:33 +0300

    lesson1

---
+++ src/derevo.java
@@ -0,0 +1,14 @@
+/**
+ * Created by User on 04.10.2016.
+ */
+class Wood
+{
+    public static void main(String[] args) {
+        Wood wood = new Wood();
+        String name = wood.getName();
+        wood.setAge(150);
+        wood.setLeaves(green, blue, yellow);
+    }
+
+
+}
